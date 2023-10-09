bddf547d7738e6143e380c24f01b8da00de92395
Variant Name <your@xmpl.com>
2023-10-09 13:13:55 +0300

    Update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Requests\Post\UpdateRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return PostResource::collection($posts);
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -24,23 +31,31 @@ class PostController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @param StoreRequest $request
+     * @return PostResource
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $data = $request->validated();
+        $post = Post::create($data);
+
+        return PostResource::make($post);
     }
 
     /**
-     * Display the specified resource.
+     * @param Post $post
+     * @return PostResource
      */
     public function show(Post $post)
     {
-        //
+        return PostResource::make($post);
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
      */
     public function edit(Post $post)
     {
@@ -48,18 +63,30 @@ class PostController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * @param UpdateRequest $request
+     * @param Post $post
+     * @return PostResource
      */
-    public function update(Request $request, Post $post)
+    public function update(UpdateRequest $request, Post $post)
     {
-        //
+        $data = $request->validated();
+        $post->update($data);
+
+        //если возвращается необновленные значения
+        $post = $post->fresh();
+
+        return PostResource::make($post);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @param Post $post
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json([
+            'message' => 'done'
+        ]);
     }
 }
